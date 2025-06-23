57875e66314b165bae801e83d7222253c0d4defe
toniles <120176462+toniles@users.noreply.github.com>
2025-06-23 11:57:11 +0200

    Create m.py

---
+++ m.py
@@ -0,0 +1,14 @@
+# PDF Form Font Size Modifier
+
+A Python script that updates the font size of all non-signature form fields in PDF files.  
+It processes all PDFs in the current folder and outputs modified versions into a `modified_pdfs` subfolder.
+
+## Requirements
+
+- Python 3.7+
+- [PyMuPDF](https://pymupdf.readthedocs.io/) (`pip install pymupdf`)
+
+## Usage
+
+```cmd
+py m.py
